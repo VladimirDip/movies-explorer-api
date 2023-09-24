8e269bb54d7cb0853694494565d45df897050971
VladimirDip <kent4594111@gmail.com>
2023-09-24 13:17:54 +1000

    fix: CORS

--- utils/configConstants.js
+++ utils/configConstants.js
@@ -6,8 +6,8 @@ const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
 const ALLOWEDCORS = [
   'http://diploma-dip.nomoredomainsicu.ru',
   'https://diploma-dip.nomoredomainsicu.ru',
-  'http://localhost:3001',
-  'https://localhost:3001',
+  'http://localhost:3000',
+  'https://localhost:3000',
 ];
 
 module.exports = {

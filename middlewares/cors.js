d519591023a05eda0023bf14c1a87d13df16eb7f
VladimirDip <kent4594111@gmail.com>
2023-09-28 06:27:08 +1000

    fix

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,21 +1,21 @@
-const {
-  ALLOWEDCORS,
-  DEFAULT_ALLOWED_METHODS,
-} = require('../utils/configConstants');
-
-module.exports = (req, res, next) => {
-  const { origin } = req.headers;
-  const { method } = req;
-  const requestHeaders = req.headers['access-control-request-headers'];
-  if (ALLOWEDCORS.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
-    res.header('Access-Control-Allow-Credentials', true);
-  }
-  if (method === 'OPTIONS') {
-    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-    res.header('Access-Control-Allow-Headers', requestHeaders);
-    res.end();
-    return;
-  }
-  next();
-};
+// const {
+//   ALLOWEDCORS,
+//   DEFAULT_ALLOWED_METHODS,
+// } = require('../utils/configConstants');
+//
+// module.exports = (req, res, next) => {
+//   const { origin } = req.headers;
+//   const { method } = req;
+//   const requestHeaders = req.headers['access-control-request-headers'];
+//   if (ALLOWEDCORS.includes(origin)) {
+//     res.header('Access-Control-Allow-Origin', origin);
+//     res.header('Access-Control-Allow-Credentials', true);
+//   }
+//   if (method === 'OPTIONS') {
+//     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+//     res.header('Access-Control-Allow-Headers', requestHeaders);
+//     res.end();
+//     return;
+//   }
+//   next();
+// };
